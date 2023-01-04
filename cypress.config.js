a4730796d0f9fcc7d538f6a8efdfd101fec2273a
Marcelo Costa <114947474+mcello23@users.noreply.github.com>
2023-01-04 12:04:18 +0100

    Modified cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    baseUrl: "http://localhost:8080",
     chromeWebSecurity: false,
     projectId: '5m2roh',
     viewportHeight: 860, 
     viewportWidth: 410,
   e2e: {
+    baseUrl: "https://cac-tat.s3.eu-central-1.amazonaws.com/index.html",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
