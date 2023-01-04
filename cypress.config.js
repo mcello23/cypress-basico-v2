1c0e607da6edf8a50d51cfcf6a20f49fd9e0b067
Marcelo Costa <114947474+mcello23@users.noreply.github.com>
2023-01-04 11:53:18 +0100

    Modified config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    baseUrl: "http://localhost:8080",
+    chromeWebSecurity: false,
     projectId: '5m2roh',
-    viewportWidth: 410,  
     viewportHeight: 860, 
+    viewportWidth: 410,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
