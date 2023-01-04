b601e247fb632ba410162719fcc8884419849efd
Marcelo Costa <114947474+mcello23@users.noreply.github.com>
2023-01-04 12:45:22 +0100

    Updates in the CI Pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    chromeWebSecurity: false,
     projectId: '5m2roh',
     viewportHeight: 860, 
     viewportWidth: 410,
   e2e: {
-    baseUrl: 'http://localhost:61566/__/#/specs/runner?file=cypress/e2e/suites_de_teste/_suite_de_testes.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
